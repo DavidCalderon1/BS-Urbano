184d13dccce6b0c904d5be0cdc12090376adabe9
t-brillyth <brillyth.forero@gmail.com>
2018-10-28 11:13:10 -0500

    agregar api gemas instaladas "rack-cors" "jwt"

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,10 @@ Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'welcome#index'
 	get '/home', to: 'home#index', as: 'home'
+
+	namespace :api, defaults: { format: 'json' } do
+		namespace :v1 do 
+			resources :users, only: [:index]
+		end
+	end
 end
