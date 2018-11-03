418ddb32016f1499654cd46c0c60ed331f2f1ce4
DavidCalderon1 <dlcalderonc@gmail.com>
2018-11-03 14:38:45 -0500

    Cambios en rutas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_225824) do
+ActiveRecord::Schema.define(version: 2018_11_03_184515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,15 @@ ActiveRecord::Schema.define(version: 2018_11_02_225824) do
     t.string "nombre"
     t.string "telefono"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "nombre"
+    t.string "imagen"
+    t.datetime "fecha"
+    t.integer "calificacion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
