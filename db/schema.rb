b40ebb6c3c62b233c6f352d635f3670fd322ce3f
DavidCalderon1 <dlcalderonc@gmail.com>
2018-11-09 17:54:14 -0500

    Cambios adicionales

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_11_07_164946) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
+  end 
 
   create_table "localities", force: :cascade do |t|
     t.string "name"
