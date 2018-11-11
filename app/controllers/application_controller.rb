8120a6f545383e4297529c6a4cd37637eaacb436
DavidCalderon1 <dlcalderonc@gmail.com>
2018-11-11 12:12:28 -0500

    Error en servidor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-	skip_before_action :verify_authenticity_token
+	
   protected
     def after_sign_in_path_for(resource)
       route_for(:home) || stored_location_for(resource) || root_path
